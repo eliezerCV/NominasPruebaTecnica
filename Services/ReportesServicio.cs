dad2d6e1f208c8ae9c6d60c2613742fa5632147b
Eliezer Cervantes <eliezer.cervantes@amher.com.mx>
2023-09-15 00:40:21 -0700

    primer ejemplo de calculo de nómina

--- Services/ReportesServicio.cs
+++ Services/ReportesServicio.cs
@@ -4,6 +4,17 @@ public class ReportesServicio {
 
   private readonly IConfiguration _configuration;
   private readonly EmpleadoServicio _empleadoServicio;
+
+  private int HorasLaborales = 8;
+  private decimal SueldoBaseHora = 30;
+  private decimal BonoPorEntrega = 5;
+  private decimal PorcentajeValeDespensa = 0.04m;
+  private int SemanasPorMes = 4;
+  private int DiasLabolaresPorSemana = 6;
+  private decimal PorcentajeISR = 0.09m;
+  private decimal PorcentajeISRAdicional = 0.3m;
+  private decimal MaximoSueloParaISR = 10000;
+
   public ReportesServicio(IConfiguration configuration, EmpleadoServicio empleadoServicio) {
     _configuration = configuration;
     _empleadoServicio = empleadoServicio;
@@ -19,19 +30,29 @@ public class ReportesServicio {
     }
     reporte.NumeroEmpleado = empleado.Numero;
     reporte.NombreEmpleado = empleado.Nombre;
-    reporte.NumeroEmpleado = empleado.Numero;
-    reporte.RolEmpleado = empleado.Rol.Nombre;
-
-    reporte.HorasTrabajadas = 0;
-    reporte.BonoPorHora = empleado.Rol.BonoPorHora;
+    
+    if (empleado.Rol != null)  {
+      reporte.BonoPorHora = empleado.Rol.BonoPorHora;
+      reporte.RolEmpleado = empleado.Rol.Nombre;
+    } else {
+      reporte.BonoPorHora = 0;
+      reporte.RolEmpleado = "Sin Rol";
+    }
+    int HorasTrabajadas = HorasLaborales * DiasLabolaresPorSemana * SemanasPorMes;
+    reporte.HorasTrabajadas = HorasTrabajadas;
     reporte.CantidadEntregas = 0;
-    reporte.BonoPorEntrega = 0;
-    reporte.ValeDespensa = 0;
-    reporte.SueldoBruto = 0;
-    reporte.ISRPorcentaje = 0;
-    reporte.ISRAplicable = 0;
-    reporte.SueldoNeto = 0;
+    reporte.BonoPorEntrega = BonoPorEntrega;
+    reporte.SueldoBruto = HorasTrabajadas * (SueldoBaseHora + reporte.BonoPorHora)/* + reporte.CantidadEntregas * BonoPorEntrega*/;
+    reporte.ValeDespensa = reporte.SueldoBruto * PorcentajeValeDespensa;
+    decimal porcentajeISRGravable = CalcularISRPorcentaje(reporte.SueldoBruto);
+    reporte.ISRPorcentaje = porcentajeISRGravable;
+    reporte.ISRAplicable = reporte.SueldoBruto * porcentajeISRGravable;
+    reporte.SueldoNeto = reporte.SueldoBruto - reporte.ISRAplicable + reporte.ValeDespensa;
 
     return reporte;
+  }
+
+  private decimal CalcularISRPorcentaje(decimal sueldoBruto) {
+    return (sueldoBruto > MaximoSueloParaISR) ? PorcentajeISR+PorcentajeISRAdicional : PorcentajeISR;
   }
 }
